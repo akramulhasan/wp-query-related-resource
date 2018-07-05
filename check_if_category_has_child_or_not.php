614022d5563cb58f32a7416f81de22bc7e8ef9b1
akramul hasan <dev.akramul@gmail.com>
2018-07-05 10:48:26 +0600

    check if any category has child or not

---
+++ check_if_category_has_child_or_not.php
@@ -0,0 +1,12 @@
+<?php 
+
+$categories = get_categories($cat);
+
+if (!empty($categories)) {
+    // This Category has children
+    }
+else {
+    // This category has no children
+    }
+
+    //The variable $cat is the category ID you are checking:
